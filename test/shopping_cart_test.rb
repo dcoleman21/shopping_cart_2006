6b42ccd03957e70808389e13a864f2fac167fb51
Dani Coleman <danicoleman00@gmail.com>
2020-07-08 11:00:13 -0600

    Create final it 4 tests with skips

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -102,4 +102,41 @@ class ShoppingCartTest < Minitest::Test
     assert_equal 43.33, cart.percentage_occupied
   end
 
+  def test_it_can_get_sorted_products_by_quantity
+    skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert_equal [product4, product1, product2, product3], cart.sorted_products_by_quantity
+  end
+
+  def test_it_can_get_a_product_breakdown
+    skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    expected = {
+      :meat=>[product2],
+      :paper=> [product1, product3],
+      :produce=> [product4]
+    }
+    assert_equal expected, cart.product_breakdown
+  end
+
 end

3d97acaa5f99a24cc5299d230a39de77aeda5233
Dani Coleman <danicoleman00@gmail.com>
2020-07-08 10:43:37 -0600

    Rework hoard method

--- lib/product.rb
+++ lib/product.rb
@@ -2,17 +2,27 @@ class Product
   attr_reader :category,
               :name,
               :unit_price,
-              :quantity
+              :quantity,
+              :hoard
 
   def initialize(category, name, unit_price, quantity)
     @category   = category
     @name       = name
     @unit_price = unit_price.to_f
     @quantity   = quantity.to_i
+    @hoard      = 0
   end
 
   def total_price
     (@unit_price * @quantity)
+  end
+
+  def is_hoarded?
+    if @quantity <= 10
+      return false
+    elsif @quantity = 10
+      return true
+    end
   end
 
 end

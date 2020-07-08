a271053ddb345e632f25e6d749ca273a96f13366
Dani Coleman <danicoleman00@gmail.com>
2020-07-08 13:00:55 -0600

    Add last two methods

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -27,11 +27,7 @@ class ShoppingCart
   end
 
   def is_full?
-    if total_number_of_products <= 13
-      return false
-    elsif total_number_of_products > 13
-      return true
-    end
+    total_number_of_products >= @capacity
   end
 
   def products_by_category(category)
@@ -41,6 +37,20 @@ class ShoppingCart
   end
 
   def percentage_occupied
-    total = (total_number_of_products / @capacity * 100).round(2)
+    (total_number_of_products / @capacity.to_f * 100).round(2)
+  end
+
+  def sorted_products_by_quantity
+    @products.sort_by do |product|
+      product.quantity
+    end.reverse
+  end
+
+  def product_breakdown
+    result = Hash.new { |hash, key| hash[key] = []}
+    @products.each do |product|
+      result[product.category] << product
+    end
+    result
   end
 end

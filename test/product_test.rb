2942090e2fa9e3f8706f0805a784fa8de84deef7
Dani Coleman <danicoleman00@gmail.com>
2020-07-08 10:33:39 -0600

    Add it 3 tests

--- test/product_test.rb
+++ test/product_test.rb
@@ -36,4 +36,22 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  # def test_hoarded_starts_false
+  #   skip
+  #   product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+  #   product2 = Product.new(:meat, 'chicken', 4.50, '2')
+  #
+  #   assert_equal false, product1.is_hoarded?
+  # end
+
+  # def test_it_can_be_hoarded
+  #   skip
+  #   product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+  #   product2 = Product.new(:meat, 'chicken', 4.50, '2')
+  #
+  #   product1.hoard
+  #
+  #   assert_equal true, product1.is_hoarded?
+  # end
 end

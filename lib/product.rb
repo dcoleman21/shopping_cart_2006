d6d7b496326c5458f13b6ff689f05d36f71cd932
Dani Coleman <danicoleman00@gmail.com>
2020-07-09 09:20:36 -0600

    Change instance variable @hoarded to @is_hoarded

--- lib/product.rb
+++ lib/product.rb
@@ -7,9 +7,9 @@ class Product
   def initialize(category, name, unit_price, quantity)
     @category   = category
     @name       = name
-    @unit_price = unit_price.to_f
-    @quantity   = quantity.to_i
-    @hoarded    = false
+    @unit_price = unit_price
+    @quantity   = quantity.to_f
+    @is_hoarded    = false
   end
 
   def total_price
@@ -17,11 +17,11 @@ class Product
   end
 
   def is_hoarded?
-    @hoarded
+    @is_hoarded
   end
 
   def hoard
-    @hoarded = true
+    @is_hoarded = true
   end
 
 end
